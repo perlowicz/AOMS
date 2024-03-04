6383be808f434f1cd2988a9b68e5e54930721e6d
perlowicz <bartek.perlinski1234@gmail.com>
2024-03-04 13:50:16 +0100

    Initial commit

---
+++ src/main/java/com/example/aoms/AomsApplication.java
@@ -0,0 +1,13 @@
+package com.example.aoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AomsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AomsApplication.class, args);
+    }
+
+}
